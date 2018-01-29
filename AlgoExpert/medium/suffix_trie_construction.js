bb50b2d4290ad03cca545990c96fa50fdb088b26
NicholasGalarza <nicogala93@gmail.com>
2018-01-28 20:51:38 -0500

    problem description

--- AlgoExpert/medium/suffix_trie_construction.js
+++ AlgoExpert/medium/suffix_trie_construction.js
@@ -1,3 +1,18 @@
+/* 
+ * Basic implementation of a suffix-trie.
+ * 
+ * Explanation:
+ * populateSuffixTrie: O(n^2) Complexity | O(n^2) Space
+ * Iterate through each letter in the initial string.
+ * Then call a subroutine to populate the trie structure. 
+ * This subroutine will hashtables with each subsequent 
+ * hastable pointing to an empty hashtable. If the next 
+ * character is not present within the subsequent node, 
+ * create the next node based of that letter. Otherwise, 
+ * assign the next letter as a branch to the subsequent 
+ * node.
+ * 
+ */
 class SuffixTrie {
   constructor(string) {
     this.root = {};
---
+++ Experience/sort_array_of_strings.js
@@ -0,0 +1,8 @@
+/* 
+ * Problem Description: 
+ * Given an array of strings, sort through them in 
+ * alphabetical order. Remove any strings that aren't
+ * characters of the alphabet.
+ */ 
+
+ 

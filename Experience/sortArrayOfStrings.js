10c2cd52207cd7dae03b8eeb23d072c493ec13f3
NicholasGalarza <nicogala93@gmail.com>
2018-01-29 17:01:37 -0500

    add: sort array of strings, and unit tests for problem

--- Experience/sortArrayOfStrings.js
+++ Experience/sortArrayOfStrings.js
@@ -3,6 +3,10 @@
  * Given an array of strings, sort through them in 
  * alphabetical order. Remove any strings that aren't
  * characters of the alphabet.
- */ 
+ */
 
- 
+const sortArrayOfStrings = (arr) => {
+  return arr
+}
+
+module.exports.sortArrayOfStrings = sortArrayOfStrings
---
+++ Experience/test/sortArrayOfStrings.js
@@ -0,0 +1,23 @@
+const { assert } = require('chai')
+const { sortArrayOfStrings } = require('../sortArrayOfStrings')
+
+describe('Sort Array of Strings', () => {
+  const test1 = ["b", "a", "e", "c", "d", "f", "g"]
+  const test2 = ["b", "a", "e", "c", "?", "d", "f", "g", ","]
+
+  it('Should return a string', () => {
+    assert.typeOf(sortArrayOfStrings(test1), 'string')
+  })
+
+  it('Should return a sorted string with no false characters', () => {
+    assert.equal(sortArrayOfStrings(test1), "abcdefg")
+  })
+
+  it('Should return a sorted string with false characters removed', () => {
+    assert.equal(sortArrayOfStrings(test2), "abcdefg")
+  })
+
+  it('Should return an empty string when there is nothing but false characters', () => {
+    assert.equal(sortArrayOfStrings(["?", ",", "%"]), "")
+  })
+})

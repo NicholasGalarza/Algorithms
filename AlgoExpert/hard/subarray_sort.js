2f1fef8fbe86681112ccaff42eabe3ca4587c085
NicholasGalarza <nicogala93@gmail.com>
2018-02-07 20:39:31 -0500

    add problem explanation for subarray sort

--- AlgoExpert/hard/subarray_sort.js
+++ AlgoExpert/hard/subarray_sort.js
@@ -27,3 +27,19 @@ function subarraySort(array) {
 
   return [leftIdxRange, rightIdxRange]
 }
+
+/* Problem Explanation
+ * Find the tentative smallest & largest values in the unordered subarray.
+ * To determine if there is an array that is unordered, ask these two questions: 
+ *  1.) Is the current element "less than" the next element?
+ *  2.) Is the current element "greater than" the previous element? 
+ * If one of these two cases is true, then the array isn't in order.
+ * 
+ * Update the minimum and maximum values by calling min() and max() on each 
+ * each tententative smallest & largest value to update the values.
+ * 
+ * To find the range, all we have to do now is use those tentative values. 
+ * To find the left index, iterate from the left until you hit the smallest value
+ * is "greater than or equal" to the array. Boom, there goes the left index. 
+ * The same logic for the right index must be applied to find the rightmost value.
+ */ 

0342527242fe464e1c4020f1c6175f70d4238693
NicholasGalarza <nicogala93@gmail.com>
2018-02-07 20:29:19 -0500

    subarray sort solution

---
+++ AlgoExpert/hard/subarray_sort.js
@@ -0,0 +1,29 @@
+function subarraySort(array) {
+  // Write your code here.
+  let orderMin = Infinity, orderMax = -Infinity
+
+  const isOutOfOrder = (idx, num, arr) => {
+    // the two if()s statements are for good practice of not
+    // accessing the array in a position which doesn't exist.
+    if (idx === 0) return num > array[idx + 1]
+    if (idx === array.length - 1) return num < array[idx - 1]
+    return num < array[idx - 1] || num > array[idx + 1]
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i]
+    if (isOutOfOrder(i, element, array)) {
+      orderMin = Math.min(orderMin, element)
+      orderMax = Math.max(orderMax, element)
+    }
+  }
+
+  if (orderMin === Infinity) return [-1, -1]
+
+  let leftIdxRange = 0, rightIdxRange = array.length - 1
+
+  while (orderMin >= array[leftIdxRange]) leftIdxRange++
+  while (orderMax <= array[rightIdxRange]) rightIdxRange--
+
+  return [leftIdxRange, rightIdxRange]
+}

cedffcd4ea34b5e7011b88e769714aa65e0c011c
NicholasGalarza <nicogala93@gmail.com>
2018-02-09 18:49:46 -0500

    update: largestRange, including optimal linear time solution

--- AlgoExpert/hard/largestRange.js
+++ AlgoExpert/hard/largestRange.js
@@ -1,4 +1,5 @@
 // Runtime O(n * lg(n)) | Space O(1)
+/*
 function largestRange(array) {
 
   array = array.sort((a, b) => a - b)
@@ -6,8 +7,9 @@ function largestRange(array) {
   let leftTick = 0, HLEFT = 0, HRIGHT = 0,
     counter = 0, highest = -Infinity
 
-  for (let curr = 0, next = curr + 1; curr < array.length - 1; curr++ , next++) {
+  for (let curr = 0; curr < array.length - 1; curr++) {
 
+    const next = curr + 1
     if (array[curr] + 1 === array[next] || array[curr] === array[next]) {
       counter++
     } else {
@@ -23,4 +25,46 @@ function largestRange(array) {
   }
 
   return [array[HLEFT], array[HRIGHT]]
+}
+*/
+
+function largestRange(array) {
+
+  let visited = {}, range = []
+
+  for (const element of array) {
+    visited[element] = false
+  }
+
+  const getRange = (value) => {
+    let temp = [], iter = value
+
+    // Check left of index, update hash, store element in array
+    while (iter in visited && visited[iter] === false) {
+      visited[iter] = true
+      temp.push(iter)
+      iter--
+    }
+
+    // Reset iter to one past the initial value
+    iter = value + 1
+
+    // Check right of index, update hash, store element in array
+    while (iter in visited && visited[iter] === false) {
+      visited[iter] = true
+      temp.push(iter)
+      iter++
+    }
+
+    return temp
+  }
+
+  for (const element of array) {
+    if (visited[element] === false) {
+      let temp = getRange(element)
+      if (temp.length >= range.length) range = temp
+    }
+  }
+
+  return [Math.min(...range), Math.max(...range)]
 }

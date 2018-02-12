9996486eb0619ead9a32b7b6a2efa907c31108d7
NicholasGalarza <nicogala93@gmail.com>
2018-02-11 23:34:19 -0500

    including linear time solution for longest substring w/o duplication

--- AlgoExpert/hard/longest_substr_wo_duplication.js
+++ AlgoExpert/hard/longest_substr_wo_duplication.js
@@ -16,3 +16,24 @@ function longestSubstringWithoutDuplication(string) {
   }
   return longest
 }
+
+// Optimal
+// Runtime O(n) | Space O(m)
+function longestSubstringWithoutDuplication(string) {
+  let lastSeen = {},
+    longestRange = [0, 1],
+    startIdx = 0
+
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i]
+
+    if (char in lastSeen) {
+      startIdx = Math.max(startIdx, lastSeen[char] + 1)
+    }
+    if (longestRange[1] - longestRange[0] < (i + 1) - startIdx) {
+      longestRange = [startIdx, i + 1]
+    }
+    lastSeen[char] = i
+  }
+  return string.slice(longestRange[0], longestRange[1])
+}

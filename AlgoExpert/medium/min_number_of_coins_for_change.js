2173e522d6364a8907722dd741b5e49344b3a663
NicholasGalarza <nicogala93@gmail.com>
2018-02-12 23:54:45 -0500

    add solution for min number of coins for change

---
+++ AlgoExpert/medium/min_number_of_coins_for_change.js
@@ -0,0 +1,16 @@
+function minNumberOfCoinsForChange(n, denoms) {
+  // Store
+  const ways = new Array(n + 1).fill(Infinity)
+  // Base Case
+  ways[0] = 0
+  // Components
+  for (const coin of denoms) {
+    for (let idx = 1; idx < n + 1; idx++) {
+      if (coin <= idx) {
+        ways[idx] = Math.min(ways[idx], ways[idx - coin] + 1)
+      }
+    }
+  }
+
+  return ways[n] !== Infinity ? ways[n] : -1
+}

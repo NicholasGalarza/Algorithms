731c91c3e33df5f7c1cb7cdfd6cb9a417f24981b
NicholasGalarza <nicogala93@gmail.com>
2018-02-24 17:40:57 -0500

    adding solution for min heap

--- AlgoExpert/medium/min_heap.js
+++ AlgoExpert/medium/min_heap.js
@@ -1,23 +1,29 @@
+/* A heap is a binary tree that has two properties:
+ * 1.) The heap is populated by level-first insertions
+ * 2.) Each child child to every node is either less than or 
+ *  greater than each child node.
+ */
 class MinHeap {
   constructor(array) {
     this.heap = this.buildHeap(array);
   }
 
+  // Runtime O(n) | Space O(n)
   buildHeap(array) {
-    const firstParent = Math.floor((array.length - 2) / 2)
+    const firstParent = this.getParent(array.length - 1)
     for (let currentIdx = firstParent; currentIdx >= 0; currentIdx--) {
       this.siftDown(currentIdx, array.length, array)
     }
     return array
   }
 
+  // Runtime O(log(n)) | Space O(1)
   siftDown(idx, heapSize, heap) {
     let childOneIdx = this.getChildOne(idx)
 
     while (childOneIdx < heapSize) {
-      const tempChildTwoIdx = this.getChildTwo(idx),
-        childTwoIdx = (tempChildTwoIdx < heapSize) ? tempChildTwoIdx : -1,
-        idxToSwap = (childTwoIdx !== -1 && heap[childTwoIdx] < heap[childOneIdx]) ? childTwoIdx : childOneIdx
+      const childTwoIdx = (this.getChildTwo(idx) < heapSize) ? this.getChildTwo(idx) : null,
+        idxToSwap = (childTwoIdx && heap[childTwoIdx] < heap[childOneIdx]) ? childTwoIdx : childOneIdx
 
       if (heap[idxToSwap] < heap[idx]) {
         this.swap(idx, idxToSwap, heap)
@@ -29,6 +35,7 @@ class MinHeap {
     }
   }
 
+  // Runtime O(log(n)) | Space O(1)
   siftUp(idx, heap) {
     let parentIdx = this.getParent(idx)
     while (idx > 0 && heap[idx] < heap[parentIdx]) {
@@ -38,8 +45,10 @@ class MinHeap {
     }
   }
 
+  // Runtime O(1) | Space O(1)
   peek() { return this.heap[0] }
 
+  // Runtime O(log(n)) | Space O(1)
   remove() {
     this.swap(0, this.heap.length - 1, this.heap)
     const deletedNode = this.heap.pop()
@@ -47,17 +56,20 @@ class MinHeap {
     return deletedNode
   }
 
+  // Runtime O(log(n)) | Space O(1)
   insert(value) {
     this.heap.push(value)
     this.siftUp(this.heap.length - 1, this.heap)
   }
 
+  // Runtime O(1) | Space O(1)
   swap(i, j, heap) {
     const temp = heap[j]
     heap[j] = heap[i]
     heap[i] = temp
   }
 
+  // Helper methods to calculate children & parent indices.
   getChildOne(idx) { return 2 * idx + 1 }
   getChildTwo(idx) { return 2 * idx + 2 }
   getParent(idx) { return Math.floor((idx - 1) / 2) }

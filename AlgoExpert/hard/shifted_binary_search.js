c260577d5c5f5c40cd11b7c75108417eb412ab18
NicholasGalarza <nicogala93@gmail.com>
2018-02-27 16:32:31 -0500

    add solution for shifted binary search

---
+++ AlgoExpert/hard/shifted_binary_search.js
@@ -0,0 +1,24 @@
+function shiftedBinarySearch(array, target) {
+  let left = 0, right = array.length - 1
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+
+    if (target === array[mid]) {
+      return mid
+    } else if (array[left] <= array[mid]) {
+      if (target < array[mid] && target >= array[left]) {
+        right = mid - 1
+      } else {
+        left = mid + 1
+      }
+    } else {
+      if (target > array[mid] && target <= array[right]) {
+        left = mid + 1
+      } else {
+        right = mid - 1
+      }
+    }
+  }
+  return -1
+}

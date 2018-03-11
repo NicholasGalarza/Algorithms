af78084ef47b88a5e3a6188bf05284929fe3af4a
NicholasGalarza <nicogala93@gmail.com>
2018-03-10 20:06:05 -0500

    update: slight fix to specs, add: solution to isOneAway solved

--- CTCI/chapter_one/one_way.js
+++ CTCI/chapter_one/one_way.js
@@ -12,11 +12,45 @@
  * @return: {boolean}
  * Runtime O(n) | Space O(1)
  */
-function isOneAway(str1, str2) {
-  let edits = 0
-  for (let i = 0; i < str1.length; i++) {
-
+function isOneAway(first, second) {
+  if (first.length === second.length) {
+    return oneEditReplace(first, second)
+  } else if (first.length + 1 === second.length) {
+    return oneEditInsert(first, second)
+  } else if (first.length - 1 === second.length) {
+    return oneEditInsert(second, first)
   }
+  return true
+}
+
+function oneEditReplace(str1, str2) {
+  let update = false
+  for (let idx = 0; idx < str1.length; idx++) {
+    if (str1[idx] != str2[idx] & update) return false
+    else if (str1[idx] != str2[idx]) update = true
+  }
+  return true
+}
+
+// @params: {string, string} s1, s2
+// @note: smaller string must be s1 and larger s2
+function oneEditInsert(s1, s2) {
+  let left = 0, right = 0,
+    inserted = false
+
+  while (left < s1.length && right < s2.length) {
+    if (s1[left] !== s2[right] && !inserted) {
+      insert = true
+      right = right + 1
+    } else if (s1[left] !== s2[right] && inserted) {
+      return false
+    } else {
+      left = left + 1
+      right = right + 1
+    }
+  }
+
+  return true
 }
 
 module.exports.isOneAway = isOneAway
--- CTCI/chapter_one/test/one_way.test.js
+++ CTCI/chapter_one/test/one_way.test.js
@@ -3,8 +3,27 @@ const { isOneAway } = require('../one_way')
 
 describe('Check if both strings are one edit or zero edits away', () => {
   const test1 = ['pale', 'ple']
-  
+  const test2 = ['pales', 'pale']
+  const test3 = ['pale', 'bale']
+  const test4 = ['pale', 'bake']
+
   it('Should return a boolean', () => {
     assert.typeOf(isOneAway(test1[0], test1[1]), 'boolean')
+  })
+
+  it(`Returns true for ${test1[0]} and ${test1[1]}`, () => {
+    assert.equal(isOneAway(test1[0], test1[1]), true)
+  })
+
+  it(`Returns true for ${test2[0]} and ${test2[1]}`, () => {
+    assert.equal(isOneAway(test2[0], test2[1]), true)
+  })
+
+  it(`Returns true for ${test3[0]} and ${test3[1]}`, () => {
+    assert.equal(isOneAway(test3[0], test3[1]), true)
+  })
+
+  it(`Returns false for ${test4[0]} and ${test4[1]}`, () => {
+    assert.equal(isOneAway(test4[0], test4[1]), false)
   })
 })

b54e1a846b865eb1ae4c90dd089431d80f1e64b6
NicholasGalarza <nicogala93@gmail.com>
2018-03-14 22:33:10 -0400

    add solution by using hash map, need to add unit tests

--- CTCI/chapter_two/remove_dups.js
+++ CTCI/chapter_two/remove_dups.js
@@ -1,3 +1,4 @@
+const { SinglyLinkedList } = require('../../DataStructures/singly_linked_list')
 /*
  * 2.1 Remove Duplicates
  * Remove duplicates from an unsorted linked list
@@ -7,11 +8,35 @@
  * @params {singly_linked_list}, list
  * @return {singly_linked_list}
  */
+const list = new SinglyLinkedList()
+list.insert(5).insert(6).insert(7).insert(6)
 
-function removeDuplicates() {
+function removeDuplicates(LL) {
+  let current = LL.head
   const set = {}
-  
+
+  while (current !== null) {
+    if (!(current.value in set)) {
+      set[current.value] = false
+    } else {
+      set[current.value] = true
+    }
+    current = current.next
+  }
+
+  current = LL.head // reset
+
+  while (current !== null) {
+    if (set[current.value]) {
+      LL.remove(current.value)
+    }
+    current = current.next
+  }
+
+  LL.printAll()
 }
+
+removeDuplicates(list)
 
 /* Without Buffer Solution
  * @params {singly_linked_list}, list

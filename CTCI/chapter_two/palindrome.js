56c67a795d8a69538b4a14d1ad0d07d2c349f5ee
NicholasGalarza <nicogala93@gmail.com>
2018-03-16 20:08:37 -0400

    add: solution for isPalinedrome

--- CTCI/chapter_two/palindrome.js
+++ CTCI/chapter_two/palindrome.js
@@ -11,6 +11,23 @@
  * Runtime O(n) | Space O(n)
  */
 
-function isPalindrome(list) { }
+function isPalindrome(list) {
+  const stack = []
+
+  let ptr, iter
+  ptr = iter = list.head
+  
+  while (ptr !== null) {
+    stack.push(ptr.value)
+    ptr = ptr.next
+  }
+ 
+  while (stack.length > 0) {
+    if (stack.pop() != iter.value) return false
+    iter = iter.next
+  }
+
+  return true
+}
 
 module.exports.isPalindrome = isPalindrome

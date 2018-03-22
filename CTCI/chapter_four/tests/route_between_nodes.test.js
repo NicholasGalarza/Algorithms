0a0f97f866430b190001cddf7e5310ada6386831
NicholasGalarza <nicogala93@gmail.com>
2018-03-22 16:08:12 -0400

    add: unit tests for route between nodes 4.1

--- CTCI/chapter_four/tests/route_between_nodes.test.js
+++ CTCI/chapter_four/tests/route_between_nodes.test.js
@@ -1,7 +1,39 @@
+const { GraphNode } =  require('../../../DataStructures/graph_node')
 const { assert } = require('chai')
 const { routeFinder } = require('../route_between_nodes')
 
 describe('Implement method to determine if route exists between two nodes', () => {
-  let test
+  const test = new GraphNode('a')
+  test.addChild('b').addChild('c')
+  test.children[0].addChild('d')
 
+  const test1 = new GraphNode('a')
+  test1.addChild('b').addChild('c')
+  test1.children[1].addChild('d').addChild('e').addChild('f')
+  test1.children[1].children[2].addChild('g')
+
+  it('returns `true` when for Node(a) and Node(c)', () => {
+    const result = routeFinder(test)
+    assert.equal(result, true)
+  })
+
+  it('returns `false` for Node(a) and Node(g)', () => {
+    const result = routeFinder(test)
+    assert.equal(result, false)
+  })
+
+  it('returns `true` for Node(a) and Node(d)', () => {
+    const result = routeFinder(test1)
+    assert.equal(result, true)
+  })
+
+  it('returns `true` for Node(a) and Node(g)', () => {
+    const result = routeFinder(test1)
+    assert.equal(result, true)
+  })
+
+  it('returns `false` for Node(a) and Node(h)', () => {
+    const result = routeFinder(test1)
+    assert.equal(result, false)
+  })
 })

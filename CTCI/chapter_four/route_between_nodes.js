f444c649352a249df84d871d5b105d1ee099c775
NicholasGalarza <nicogala93@gmail.com>
2018-03-22 16:55:55 -0400

    add: unoptimized soluton for route between nodes

--- CTCI/chapter_four/route_between_nodes.js
+++ CTCI/chapter_four/route_between_nodes.js
@@ -4,8 +4,24 @@
  * out whether there is a route between two nodes.
  */
 
-function routeFinder(graph) {
-  
+/*
+ * @params {node, string, string} graph, start, end
+ * @return {boolean}
+ */
+function routeFinder(graph, start, end) {
+  const nodes = depthFirstTraverse(graph, [], [start, end])
+  return nodes.includes(start) && nodes.includes(end)
+}
+
+function depthFirstTraverse(root, array = [], values) {
+  if (root === null) return
+
+  array.push(root.value)
+  for (const child of root.children) {
+    depthFirstTraverse(child, array)
+  }
+
+  return array
 }
 
 module.exports.routeFinder = routeFinder
--- CTCI/chapter_four/tests/route_between_nodes.test.js
+++ CTCI/chapter_four/tests/route_between_nodes.test.js
@@ -1,8 +1,9 @@
-const { GraphNode } =  require('../../../DataStructures/graph_node')
+const { GraphNode } = require('../../../DataStructures/graph_node')
 const { assert } = require('chai')
 const { routeFinder } = require('../route_between_nodes')
 
 describe('Implement method to determine if route exists between two nodes', () => {
+
   const test = new GraphNode('a')
   test.addChild('b').addChild('c')
   test.children[0].addChild('d')
@@ -13,27 +14,27 @@ describe('Implement method to determine if route exists between two nodes', () =
   test1.children[1].children[2].addChild('g')
 
   it('returns `true` when for Node(a) and Node(c)', () => {
-    const result = routeFinder(test)
+    const result = routeFinder(test, 'a', 'c')
     assert.equal(result, true)
   })
 
   it('returns `false` for Node(a) and Node(g)', () => {
-    const result = routeFinder(test)
+    const result = routeFinder(test, 'a', 'g')
     assert.equal(result, false)
   })
 
   it('returns `true` for Node(a) and Node(d)', () => {
-    const result = routeFinder(test1)
+    const result = routeFinder(test1, 'a', 'd')
     assert.equal(result, true)
   })
 
   it('returns `true` for Node(a) and Node(g)', () => {
-    const result = routeFinder(test1)
+    const result = routeFinder(test1, 'a', 'g')
     assert.equal(result, true)
   })
 
   it('returns `false` for Node(a) and Node(h)', () => {
-    const result = routeFinder(test1)
+    const result = routeFinder(test1, 'a', 'h')
     assert.equal(result, false)
   })
 })

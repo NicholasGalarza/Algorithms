b72c08baa36a1ab4b8cfa35070e1e32a48413d23
NicholasGalarza <nicogala93@gmail.com>
2018-03-27 13:00:34 -0400

    add: untests solution for list_of_depths problem

---
+++ CTCI/chapter_four/list_of_depth.js
@@ -0,0 +1,50 @@
+/*
+ * 4.3 List of Depths
+ * Given a binary tree, design an algorithm which creates
+ * a linked list of all the nodes at each depth (e.g., if
+ * you have a tree with depth D, you'll have D linked 
+ * lists)
+ */
+
+/*
+ * Runtime O(n) | Space O(n)
+ */
+function listOfDepths(tree) {
+  return traverseAndLink(0, tree, {})
+}
+
+// preorder traverse and build or add elements to a
+// linked list at each level
+function traverseAndLink(level, root, container) {
+  if (root === null) return container
+
+  if (!(level in container)) {
+    container[level] = new SinglyLinkedList(root.value)
+  } else {
+    container[level].insert(root.value)
+  }
+
+  traverseAndLink(level + 1, root.left, container)
+  traverseAndLink(level + 1, root.right, container)
+
+  return container
+}
+
+class SinglyLinkedList {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+
+  insert(value) {
+    const node = new SinglyLinkedList(value)
+    let current = this
+    while (current.next !== null) {
+      current = current.next
+    }
+    current.next = node
+    return this
+  }
+}
+
+module.exports.listOfDepths = listOfDepths
